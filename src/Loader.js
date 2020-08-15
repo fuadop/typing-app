2897a2bc9d2934d2619f7e9630f89a9c0eb3213c
fuadop <fuadolatunji@gmail.com>
2020-08-15 14:20:53 +0100

    init commit😤

---
+++ src/Loader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+const Loader = ()=>(
+    <div className="loadingio-spinner-pulse-tyaqiwmtt9"><div className="ldio-vyciikf06pm">
+    <div></div><div></div><div></div>
+    </div></div>
+);
+
+export default Loader;
